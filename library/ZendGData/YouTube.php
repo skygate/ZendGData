048b71d8be2e9fbdcbf2698ddc37567267a1bd1a
Pawel Skotnicki <pawel.skotnicki@wp.pl>
2013-01-18 13:35:15 +0100

    Set default major protocol version to 2 for YouTube

--- library/ZendGData/YouTube.php
+++ library/ZendGData/YouTube.php
@@ -23,6 +23,7 @@ use Zend\Stdlib\ErrorHandler;
  */
 class YouTube extends Media
 {
+    const DEFAULT_MAJOR_PROTOCOL_VERSION = 2;
 
     const AUTH_SERVICE_NAME = 'youtube';
     const CLIENTLOGIN_URL = 'https://www.google.com/youtube/accounts/ClientLogin';
@@ -149,6 +150,16 @@ class YouTube extends Media
 
         // NOTE This constructor no longer calls the parent constructor
         $this->setHttpClient($client, $applicationId, $clientId, $developerKey);
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see ZendGData.App::applyDefaults()
+     */
+    public function applyDefaults()
+    {
+        $this->setMajorProtocolVersion(self::DEFAULT_MAJOR_PROTOCOL_VERSION);
+        $this->setMinorProtocolVersion(self::DEFAULT_MINOR_PROTOCOL_VERSION);
     }
 
     /**
